6aa48d5dd91e28d036348c9e156f9c4968beb327
underground0930 <go.nishiduka.1985@gmail.com>
2023-07-14 13:23:49 +0900

    feat: ログイン、ログアウトのロジックを実装

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,3 +1,20 @@
 class UserSessionsController < ApplicationController
   def new; end
+
+  def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to("/", success: "ログインに成功しました")
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+    
+  end
+
+  def destroy
+    logout
+    redirect_to("/", success: "ログアウトしました", status: :see_other)
+  end
+  
 end
