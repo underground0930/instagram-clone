6eff87e33137bc15e63e9b1a8a56d41f3cea2241
underground0930 <go.nishiduka.1985@gmail.com>
2023-07-23 16:50:49 +0900

    spec を update

--- spec/system/signup_spec.rb
+++ spec/system/signup_spec.rb
@@ -26,36 +26,27 @@ RSpec.describe "Signup", type: :system do
       visit signup_path
       user_attributes = FactoryBot.attributes_for(:user)
 
-      click_button "新規登録"
-      within ".alert.alert-danger" do
-        expect(page.all('li').count).to_not eq 0
-      end
+      submit_and_count_errors
 
       fill_in name: "user[username]",	with: user_attributes[:username] 
-      click_button "新規登録"
-      within ".alert.alert-danger" do
-        expect(page.all('li').count).to_not eq 0
-      end
+      submit_and_count_errors
 
       fill_in name: "user[email]",	with: user_attributes[:email] 
-      click_button "新規登録"
-      within ".alert.alert-danger" do
-        expect(page.all('li').count).to_not eq 0
-      end
+      submit_and_count_errors
 
       fill_in name: "user[password]",	with: user_attributes[:password] 
-      click_button "新規登録"
-      within ".alert.alert-danger" do
-        expect(page.all('li').count).to_not eq 0
-      end
+      submit_and_count_errors
 
       fill_in name: "user[password_confirmation]",	with: user_attributes[:password_confirmation] 
-      click_button "新規登録"
-      within ".alert.alert-danger" do
-        expect(page.all('li').count).to_not eq 0
-      end
-
+      submit_and_count_errors
     end
 
+  end
+
+  def submit_and_count_errors
+    click_button "新規登録"
+    within ".alert.alert-danger" do
+      expect(page.all('li').count).to_not eq 0
+    end  
   end
 end
