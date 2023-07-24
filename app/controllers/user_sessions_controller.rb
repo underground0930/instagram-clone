3387c75dd84d8a08b3cd8397b716bdacdb07c6fe
underground0930 <go.nishiduka.1985@gmail.com>
2023-07-24 09:40:01 +0900

    style: rubocop -A

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,7 +5,7 @@ class UserSessionsController < ApplicationController
   def new; end
 
   def create
-    @user = login(params[:email], params[:password], params[:remember_check] == "1" ? true : false)
+    @user = login(params[:email], params[:password], params[:remember_check] == '1')
     if @user
       redirect_back_or_to('/', success: t('controllers.user_sessions.create.success'))
     else
