2aa0fa26f85832ba2c178955a7b12bb29c90810e
underground0930 <go.nishiduka.1985@gmail.com>
2023-07-29 14:35:29 +0900

    add: commentのsystem スペックを追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "Comments", type: :system do
     context "ログインした状態" do
       before do
         log_in(user_attributes)
+        visit root_path
       end
 
       it "新規登録出来ること" do
@@ -28,9 +29,9 @@ RSpec.describe "Comments", type: :system do
         comment = FactoryBot.create(:comment, user:user, post:post) 
         visit post_path(post)
         within "#comment_#{comment.id}" do
-          click_button "編集"
+          click_on "編集"
           fill_in name: "comment[body]", with: "更新コメントです"
-          click_button "更新する"
+          click_on "更新する"
           expect(page).to have_content "更新コメントです"
         end
       end
@@ -44,22 +45,41 @@ RSpec.describe "Comments", type: :system do
       end
   
       it "自分のコメントは削除が出来ること" do
+        comment = FactoryBot.create(:comment, user:user, post:post) 
+        visit post_path(post)
+        within "#comment_#{comment.id}" do
+          page.accept_confirm { click_on "削除" }
+        end
+        expect(page).to_not have_css("#comment_#{comment.id}")
       end
 
       it "他人のコメントは更新と削除のボタンが出ないこと" do
+        comment = FactoryBot.create(:comment, user:user2, post:post) 
+        visit post_path(post)
+        within "#comment_#{comment.id}" do
+          expect(page).to_not have_content "編集"
+          expect(page).to_not have_content "削除"
+        end
       end
     
     end
       
-    # context "ログインしていない状態" do
-    #   it "新規登録フォームが表示されないこと" do
+    context "ログインしていない状態" do
+      let(:comment) { FactoryBot.create(:comment, user:user, post:post) }
+      it "新規登録フォームが表示されないこと" do
+        visit post_path(post)
+        expect(page).to_not have_css("#form_comment")
+      end
 
-    #   end
+      it "更新と削除のボタンが出ないこと" do
+        visit post_path(post)
+        within "#comments" do
+          expect(page).to_not have_content "編集"
+          expect(page).to_not have_content "削除"          
+        end
+      end
 
-    #   it "更新と削除のボタンが出ないこと" do
-    #   end
-
-    # end
+    end
 
   end
   
