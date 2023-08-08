4f8c396fffb4c9cac327e6ba803d4be51625fa17
underground0930 <go.nishiduka.1985@gmail.com>
2023-08-08 21:06:51 +0900

    modify: Relationships controllerにcreate,destroyアクションを追加

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,2 +1,14 @@
 class Users::RelationshipsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+  end
+
 end
