490164b1401ba55c9c332a7602affe62c00ca285
underground0930 <go.nishiduka.1985@gmail.com>
2023-08-08 12:47:23 +0900

    modify: userモデルのrelationshipモデルとのリレーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
