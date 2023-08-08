8f225f6b76f85c4ed53c7258666ab53dfb5bc001
underground0930 <go.nishiduka.1985@gmail.com>
2023-08-08 22:44:39 +0900

    refactor: linter

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -5,10 +5,9 @@ class Users::RelationshipsController < ApplicationController
     @user = User.find(params[:user_id])
     current_user.follow(@user)
   end
-  
+
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_posts, through: :likes, source: :post
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id'
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id'
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
