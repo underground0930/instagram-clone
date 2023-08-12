df3a88cf48230d994701a2db6a6cc4299772726d
underground0930 <go.nishiduka.1985@gmail.com>
2023-08-12 15:29:18 +0900

    refactor: rubocop & erblint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
     @pagy, @users = pagy(User.order(created_at: :desc), items: 5)
   end
 
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
-    @pagy, @posts = pagy(@user.posts.with_attached_images.order(created_at: :desc), items:10)
+    @pagy, @posts = pagy(@user.posts.with_attached_images.order(created_at: :desc), items: 10)
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
