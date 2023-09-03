6005537918e1cf8955eba8f5084dc102a20a3259
underground0930 <go.nishiduka.1985@gmail.com>
2023-09-03 14:17:43 +0900

    add: modelの設定

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -9,4 +9,17 @@
 #  updated_at :datetime         not null
 #
 class Notification < ApplicationRecord
+  has_many :user_notifications, depedent: :destroy
+  has_many :users, through: :user_notifications
+
+  validates :title, presence: true
+  validates :url, presence: true, format: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/
+
+  def notify(*user)
+    users = user.flatten
+    user_notifications = users.pluck(:id).map do |user_id|
+      { notification_id: id, user_id: }
+    end
+    UserNotification.insert_all(user_notifications)
+  end
 end
--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -23,4 +23,10 @@
 class UserNotification < ApplicationRecord
   belongs_to :notification
   belongs_to :user
+
+  validates :read, inclusion: { in: [true, false] }
+  validates :notification_id, uniqueness: { scope: :user_id }
+
+  delegate :title, to: :notification, prefix: true
+  delegate :url, :notification, prefix: true
 end
